0e546a053602730046c02a433b4eb4f0dbee0174
George Tsiftsis <gtsiff@gmail.com>
2021-08-27 19:57:10 +0300

    onephoto

--- pages/Egkatastaseis-tae-kwon-do/data.js
+++ pages/Egkatastaseis-tae-kwon-do/data.js
@@ -10,14 +10,14 @@
 export const _IMAGES = [
   {
     original: "https://firebasestorage.googleapis.com/v0/b/initialise-6d68a.appspot.com/o/Gallery%2Fekswterikosxorosmakdeoniki.jpg?alt=media&token=8baa53ad-d1d0-4178-97f7-2758576a1e2e",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    //     thumbnail: "https://picsum.photos/id/1018/250/150/",
+    //   },
+    //   {
+    //     original: "https://picsum.photos/id/1015/1000/600/",
+    //     thumbnail: "https://picsum.photos/id/1015/250/150/",
+    //   },
+    //   {
+    //     original: "https://picsum.photos/id/1019/1000/600/",
+    //     thumbnail: "https://picsum.photos/id/1019/250/150/",
   },
 ];
