f6fa09a22d3ef818b484329c81803809ae7f9144
George Tsiftsis <gtsiff@gmail.com>
2021-08-31 06:19:57 +0300

    hide keys another way

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const webpack = require("webpack");
-const { parsed: myEnv } = require("dotenv").config();
+// const { parsed: myEnv } = require("dotenv").config();
 
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    return config;
+  env: {
+    customKey: "AIzaSyD8PhnFnWcQnL8Qz2m9s8tKThO1IKZuBvg",
+    gridCustomKey: "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",
   },
-  future: { webpack5: true },
+  // webpack(config) {
+  //   config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+  //   return config;
+  // },
 };
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,8 @@
 const mail = require("@sendgrid/mail");
-mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+// mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+mail.setApiKey(process.env.gridCustomKey);
 
+// {process.env.gridCustomKey}
 export default async (req, res) => {
   const body = JSON.parse(req.body);
 
