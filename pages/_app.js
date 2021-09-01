821e84d82962103c1b6e8097d80f4dad67245e2b
George Tsiftsis <gtsiff@gmail.com>
2021-09-01 06:49:55 +0300

    metatags

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import "../styles/globals.css";
 // // import Document, { Html, Head, Main, NextScript } from "next/document";
 import Layout from "../components/layout/Layout";
 // import { Html } from "next/document";
-// import Head from "next/head";
+import Head from "next/head";
 import Error from "next/error";
-import { AnimatePresence, motion } from "framer-motion";
+// import { AnimatePresence, motion } from "framer-motion";
 
 // // import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,20 +14,18 @@ function MyApp({ Component, pageProps }) {
   }
   return (
     // <Html lang="el">
-    //   <Head>
-    //     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    //     <meta http-Equiv="Content-Type" content="text/html; charset=utf-8" />
-    //     </Head>
-    /* <body> */
-    <Layout>
-      {/* <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}> */}
-      <Component {...pageProps} />
-      {/* </AnimatePresence> */}
-    </Layout>
-    /* <Main />
-          <NextScript /> */
-    // {/* </body> */}
-    // </Html>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-Equiv="Content-Type" content="text/html; charset=utf-8" />
+      </Head>
+
+      <Layout>
+        {/* <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}> */}
+        <Component {...pageProps} />
+        {/* </AnimatePresence> */}
+      </Layout>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang="el">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" /> */}
         </Head>
         <body>
           <Main />
