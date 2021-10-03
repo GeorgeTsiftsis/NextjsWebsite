2ddcee4cd447f238da04579150ede451c06b83f0
George Tsiftsis <gtsiff@gmail.com>
2021-10-03 17:04:40 +0300

    New reverse anouncement

--- components/Anouncements/SingleAnouncement.js
+++ components/Anouncements/SingleAnouncement.js
@@ -16,17 +16,17 @@ function SingleAnouncement(props) {
       <p className={classes.title}>{props.anouncement.title}</p>
       <p className={classes.date_of_anouncements}> {props.anouncement.date}</p>
       <div className={classes.divphoto}>
-        <img className={classes.photo} src={props.anouncement.photo} alt={props.anouncement.title}></img>
 
         <p className={classes.description}> {props.anouncement.full_description}</p>
-        <FacebookShareButton
+        <img className={classes.photo} src={props.anouncement.photo} alt={props.anouncement.title}></img>
+        </div>
+        <FacebookShareButton  className={classes.frop}
   url={`https://nextjs-website-hmbsrp9yr-georgetsiftsis.vercel.app/announcement/${props.anouncement.id}`}
   quote={props.anouncement.title}
   
 >
-  <FacebookIcon size={32} round />
+  <FacebookIcon size={40} round />
 </FacebookShareButton>
-      </div>
     </div>
     </>
   );
