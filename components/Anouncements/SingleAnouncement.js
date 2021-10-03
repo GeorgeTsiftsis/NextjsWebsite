c731d7f7cc83f195123e2e8f38e7f80e2c758896
George Tsiftsis <gtsiff@gmail.com>
2021-10-03 20:34:02 +0300

    Create single anouncement

--- components/Anouncements/SingleAnouncement.js
+++ components/Anouncements/SingleAnouncement.js
@@ -13,21 +13,26 @@ function SingleAnouncement(props) {
       <meta name="description" content={props.anouncement.title} />
       </Head>
     <div className={classes.SingleAnouncement}>
+      
+      <div>
       <p className={classes.title}>{props.anouncement.title}</p>
       <p className={classes.date_of_anouncements}> {props.anouncement.date}</p>
       <div className={classes.divphoto}>
-
-        <p className={classes.description}> {props.anouncement.full_description}</p>
         <img className={classes.photo} src={props.anouncement.photo} alt={props.anouncement.title}></img>
+        </div> 
+        <p className={classes.description}> {props.anouncement.full_description}</p>
         </div>
-        <FacebookShareButton  className={classes.frop}
-  url={`https://nextjs-website-hmbsrp9yr-georgetsiftsis.vercel.app/announcement/${props.anouncement.id}`}
+   
+      <div className={classes.fb}>
+        <FacebookShareButton url={`https://nextjs-website-hmbsrp9yr-georgetsiftsis.vercel.app/announcement/${props.anouncement.id}`}
   quote={props.anouncement.title}
   
 >
   <FacebookIcon size={40} round />
-</FacebookShareButton>
-    </div>
+</FacebookShareButton> 
+</div>
+</div>
+    
     </>
   );
 }
