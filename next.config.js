83dceb78b84029f61264e072a58507d721229b63
George Tsiftsis <gtsiff@gmail.com>
2021-10-18 02:41:37 +0300

    Remove Unessecary things

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ module.exports =   withOptimizedImages({
   reactStrictMode: true,
   images: {
     domains: ["firebasestorage.googleapis.com"],
-  },
-  env: {
-    customKey: "AIzaSyD8PhnFnWcQnL8Qz2m9s8tKThO1IKZuBvg",
-    gridCustomKey: "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",
-  },
+  }
 
 });
