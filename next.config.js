2fcd7aff60f34b881432e1daee68fecd8c31de45
George Tsiftsis <gtsiff@gmail.com>
2021-12-15 14:34:27 +0200

    Delete unessecary code

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
-
 const webpack = require("webpack");
 // const { parsed: myEnv } = require("dotenv").config();
-const withOptimizedImages = require('next-optimized-images');
- 
-module.exports =   withOptimizedImages({
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
   reactStrictMode: true,
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  env: {
-    customKey: "AIzaSyD8PhnFnWcQnL8Qz2m9s8tKThO1IKZuBvg",
-    gridCustomKey: "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",
-  },
-
 });
