123cdacdb4032f5090ce3435c4d9d0aa1e996652
George Tsiftsis <gtsiff@gmail.com>
2021-12-29 20:25:18 +0200

    utilise env to secure firebase

--- components/util/firebase.js
+++ components/util/firebase.js
@@ -5,14 +5,14 @@ import "firebase/storage";
 if (!firebase.apps.length) {
   // Initialize Firebase
   var firebaseConfig = {
-    apiKey: "AIzaSyDE75FtHrwEZSsrXPkDV_WnvzqUu2jxBwE",
-    authDomain: "initialise-6d68a.firebaseapp.com",
-    databaseURL: "https://initialise-6d68a-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "initialise-6d68a",
-    storageBucket: "initialise-6d68a.appspot.com",
-    messagingSenderId: "861841404703",
-    appId: "1:861841404703:web:171266340b7bd049e5f62a",
-    measurementId: "G-S6X0HHJ5RL",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
   };
   firebase.initializeApp(firebaseConfig);
 } else {
--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -15,40 +15,69 @@ const variants = {
 };
 
 const SimpleMap = (props) => {
-  const [center, setCenter] = useState({ lat: 40.3006998281867, lng: 21.78543607992064 });
+  const [center, setCenter] = useState({
+    lat: 40.3006998281867,
+    lng: 21.78543607992064,
+  });
   const [zoom, setZoom] = useState(16);
   const defaultOptions = { scrollwheel: false };
-  const api_key = process.env.customKey;
+  const api_key = process.env.CUSTOMKEY;
 
   {
     return (
       <>
-      <Head>
-      <title> Α.Σ. Μακεδονική Δύναμη Κοζάνης Επικοινωνία </title>
-      <meta name="description" content="Πληροφοριες Επικοινωνίας για την Μακεδονικη Δύναμη Κοζάνης" />
-      </Head>
-      <motion.section style={{ backgroundColor: "#6096ba" }} initial="hidden" animate="enter" exit="exit" variants={variants} transition={{ type: "linear" }}>
-        <HorizontalLine title={"Πληροφορίες"} />
-        <div className={classes.container_details_contact}>
-          <p className={classes.text_details}>
-            Η έδρα του συλλόγου βρίσκεται επί της οδού Εστίας 3, ΤΚ 50131 Κοζάνη. Επικοινωνήστε μαζί μας για οποιαδήποτε πληροφορία/απορία μέσω της παρακάτω ηλεκτρονικής φόρμας ή να μας καλέσετε στο <strong style={{ display: "block" }}>Σταθερό: 24610 25054</strong> <strong> Κινητό: 6946504020</strong>.
-          </p>
-        </div>
-
-        <div className={classes.bigDiv}>
-          <FormforContact />
-          <div className={classes.SceduleContact}>
-            <TimeScedule style={{ alignSelf: "end" }} />
-            <TelContact />
+        <Head>
+          <title> Α.Σ. Μακεδονική Δύναμη Κοζάνης Επικοινωνία </title>
+          <meta
+            name="description"
+            content="Πληροφοριες Επικοινωνίας για την Μακεδονικη Δύναμη Κοζάνης"
+          />
+        </Head>
+        <motion.section
+          style={{ backgroundColor: "#6096ba" }}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          variants={variants}
+          transition={{ type: "linear" }}
+        >
+          <HorizontalLine title={"Πληροφορίες"} />
+          <div className={classes.container_details_contact}>
+            <p className={classes.text_details}>
+              Η έδρα του συλλόγου βρίσκεται επί της οδού Εστίας 3, ΤΚ 50131
+              Κοζάνη. Επικοινωνήστε μαζί μας για οποιαδήποτε πληροφορία/απορία
+              μέσω της παρακάτω ηλεκτρονικής φόρμας ή να μας καλέσετε στο{" "}
+              <strong style={{ display: "block" }}>Σταθερό: 24610 25054</strong>{" "}
+              <strong> Κινητό: 6946504020</strong>.
+            </p>
           </div>
-        </div>
 
-        <div style={{ height: "400px", width: "100%", padding: "1rem" }}>
-          <GoogleMapReact bootstrapURLKeys={{ key: `${api_key}` }} defaultCenter={center} defaultZoom={zoom} defaultOptions={defaultOptions}>
-            <Marker lat={40.3006998281867} lng={21.78543607992064} name="Εστίας 3, Πάτηστε για οδηγίες" color="red" />
-          </GoogleMapReact>
-        </div>
-      </motion.section>
+          <div className={classes.bigDiv}>
+            <FormforContact />
+            <div className={classes.SceduleContact}>
+              <TimeScedule style={{ alignSelf: "end" }} />
+              <TelContact />
+            </div>
+          </div>
+
+          <div style={{ height: "400px", width: "100%", padding: "1rem" }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: `${api_key}`,
+              }}
+              defaultCenter={center}
+              defaultZoom={zoom}
+              defaultOptions={defaultOptions}
+            >
+              <Marker
+                lat={40.3006998281867}
+                lng={21.78543607992064}
+                name="Εστίας 3, Πάτηστε για οδηγίες"
+                color="red"
+              />
+            </GoogleMapReact>
+          </div>
+        </motion.section>
       </>
     );
   }
