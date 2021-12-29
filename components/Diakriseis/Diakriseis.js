759225aabf5c053c0312f8cc770ed49be9c7e9e7
George Tsiftsis <gtsiff@gmail.com>
2021-12-29 20:26:47 +0200

    Just prettify

--- components/Diakriseis/Diakriseis.js
+++ components/Diakriseis/Diakriseis.js
@@ -1,19 +1,23 @@
-import classes from './style.module.css'
-import DiakrisiItem from '../DiakrisiItem/DiakrisiItem';
+import classes from "./style.module.css";
+import DiakrisiItem from "../DiakrisiItem/DiakrisiItem";
 function Diakriseis(props) {
-
-    const diakriseisList = props.diakriseis.map(function (diakrisi) {
-      return <DiakrisiItem diakrisi={diakrisi} key={diakrisi.id} />;
-    });
-    return (
-        <div className={classes.container}>
-            <h1 className={classes.title}>Διακρίσεις Αθλητών</h1>
+  const diakriseisList = props.diakriseis.map(function (diakrisi) {
+    return <DiakrisiItem diakrisi={diakrisi} key={diakrisi.id} />;
+  });
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.title}>Διακρίσεις Αθλητών</h1>
       <span className={classes.line}></span>
-      <img className={classes.image} alt="Διακρίσεις Μακεδονικής Δύναμης" src="/DiakriseisAthlitwn.jpg"></img>
-<h2 className={classes.title2}>Παγκόσμιες Διακρίσεις</h2>
-<span className={classes.line}></span>
-       <div> {diakriseisList}</div> </div>
-    )
+      <img
+        className={classes.image}
+        alt="Διακρίσεις Μακεδονικής Δύναμης"
+        src="/DiakriseisAthlitwn.jpg"
+      ></img>
+      <h2 className={classes.title2}>Παγκόσμιες Διακρίσεις</h2>
+      <span className={classes.line}></span>
+      <div> {diakriseisList}</div>
+    </div>
+  );
 }
 
-export default Diakriseis
+export default Diakriseis;
