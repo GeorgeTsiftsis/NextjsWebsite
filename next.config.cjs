12cb6187fa5531a99913e98009cf499d4c77fcad
Geroge Tsiftsis <gtsiff@gmail.com>
2023-11-06 23:55:01 +0200

    test with no export

--- next.config.cjs
+++ next.config.cjs
@@ -1,19 +1,32 @@
 // const { parsed: myEnv } = require("dotenv").config();
 // import withOptimizedImages from "next-optimized-images";
 
-export default withOptimizedImages({
-  reactStrictMode: true,
+// export default withOptimizedImages({
+//   reactStrictMode: true,
+//   images: {
+//     // remotePatterns: [ {
+//     //   protocol: 'https',
+//       domains: ["firebasestorage.googleapis.com"],
+//       // pathname: '**',
+
+  
+//     // }]
+//   },
+//   build: {
+
+//     commonjsOptions: { include: [] },
+//   }
+// });
+
+module.exports = {
   images: {
-    // remotePatterns: [ {
-    //   protocol: 'https',
-      domains: ["firebasestorage.googleapis.com"],
-      // pathname: '**',
-
-
-    // }]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "firebasestorage.googleapis.com",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+    ],
   },
-  build: {
-
-    commonjsOptions: { include: [] },
-  }
-});
+}
